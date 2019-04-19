9d33a62836823c5915d8a9a0c2f08fdfd3b7127d
Jictyvoo <jictyvoo.ecomp@gmail.com>
2019-04-19 15:30:21 -0300

    Added parse to open comment and close comment, now can parse a js script without think that is a open-tag inside then

--- src/main.lua
+++ src/main.lua
@@ -1,4 +1,4 @@
---[[ This is a test file, so use this compiled version to test the funcionality --]]
+--[[ This is a test file, so use this compiled version to test the funcionality only --]]
 local parserHTML = require("models.ParserHTML")
 parserHTML = parserHTML(true, arg[1])
 local file = io.open("./output.debug", "w")
--- src/models/ParserHTML.lua
+++ src/models/ParserHTML.lua
@@ -15,7 +15,7 @@ function ParserHTML:new(data)
     return setmetatable({
         lexycalAttributes = {
             openTag = false, openString = false, current = {}, isAttribute = false,
-            previous = nil, tagLexeme = {}, tagInfo = nil, lineCount = 1
+            previous = nil, tagLexeme = {}, tagInfo = nil, lineCount = 1, openComment = false
         },
         data = data, result = {}, tokenList = {}, filename = "direct-parse",
         tags = {}, ids = {}
@@ -42,6 +42,7 @@ function ParserHTML:writeTokenEnd(column)
         if self.lexycalAttributes.tagInfo.name == "" then
             self.lexycalAttributes.tagInfo.name = name
             local tagType = name:sub(1, 3) == "!--" and "open-comment" or name:sub(1, 1) == "/" and "close-tag" or "open-tag"
+            if tagType == "open-comment" then self.lexycalAttributes.openComment = true end
             table.insert(self.tokenList, Token(name, tagType, self.lexycalAttributes.lineCount, column, self.filename))
         else
             self.lexycalAttributes.tagInfo.attributes[name] = ""
@@ -60,11 +61,37 @@ function ParserHTML:writeTagContent()
     end
 end
 
+function ParserHTML:verifyEndComment(remove)
+    local last = self.lexycalAttributes.tagLexeme[#self.lexycalAttributes.tagLexeme]
+    local penultimate = self.lexycalAttributes.tagLexeme[#self.lexycalAttributes.tagLexeme - 1]
+    local isEndComment = false
+    if last == "-" and penultimate == "-" then
+        isEndComment = true
+    end
+    if isEndComment and remove then
+        table.remove(self.lexycalAttributes.tagLexeme, #self.lexycalAttributes.tagLexeme)
+        table.remove(self.lexycalAttributes.tagLexeme, #self.lexycalAttributes.tagLexeme)
+    end
+    return isEndComment
+end
+
 function ParserHTML:deepParse(data)
     --[[ verifying if is a tag start--]]
     local splited = self:splitString(data)
     for _, word in pairs(splited) do
-        if self.lexycalAttributes.openTag then
+        if self.lexycalAttributes.openComment then
+            if word == ">" then
+                local isEndComment = self:verifyEndComment(true)
+                if isEndComment then
+                    table.insert(self.tokenList, Token(tableString(self.lexycalAttributes.tagLexeme), "comment-content", self.lexycalAttributes.lineCount, _, self.filename))
+                    table.insert(self.tokenList, Token("--", "end-comment", self.lexycalAttributes.lineCount, _, self.filename))
+                    self.lexycalAttributes.tagLexeme = {}
+                    self.lexycalAttributes.openComment = false; self.lexycalAttributes.openTag = false
+                end
+            else
+                table.insert(self.lexycalAttributes.tagLexeme, word)
+            end
+        elseif self.lexycalAttributes.openTag then
             local isQuotation = (word == "\"" or word == "\'")
             --[[ Here's the code to identify strings in attributes --]]
             if isQuotation or self.lexycalAttributes.openString then
@@ -92,7 +119,7 @@ function ParserHTML:deepParse(data)
                 end
             end
         else
-            if word == "<" then
+            if word == "<" and splited[_ + 1] ~= string.char(9) and splited[_ + 1] ~= string.char(32) then
                 self:writeTagContent()
                 self.lexycalAttributes.openTag = true
                 self:createTagInfo()
